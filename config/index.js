484307a6301c1d24e9e0b6c653c5bdd7be3434fe
hatuw <jiaxi_wu@ieee.org>
2018-09-14 20:40:10 +0800

    update config

--- config/index.js
+++ config/index.js
@@ -4,19 +4,22 @@ module.exports = {
         contractAddress: {},
     },
     livenet_private: {
-        url: 'https://sidechain.livenet.sonm.com',
+        // url: 'https://sidechain.livenet.sonm.com',
+        url: 'http://222.200.180.185:8545',
         contractAddress: {
-            addressRegistry: '0xd1a6f3d1ae33b4b19565a6b283d7a05c5a0decb0',
+            addressRegistry: '0xc8fed8603e0e6bf0d9a6b6c9b89606135f60ed71',
         },
     },
     rinkeby: {
-        url: 'https://rinkeby.infura.io',
+        // url: 'https://rinkeby.infura.io',
+        url: 'http://222.200.180.184:8545',
         contractAddress: {},
     },
     rinkeby_private: {
-        url: 'https://sidechain-dev.sonm.com',
+        // url: 'https://sidechain-dev.sonm.com',
+        url: 'http://222.200.180.185:8545',
         contractAddress: {
-            addressRegistry: '0x79b084653ca2588ed3915159e368db58aef165ee',
+            addressRegistry: '0xc8fed8603e0e6bf0d9a6b6c9b89606135f60ed71',
         },
     },
     testrpc: {
@@ -26,7 +29,7 @@ module.exports = {
     testrpc_private: {
         url: 'https://proxy.test.sonm.com:8546',
         contractAddress: {
-            addressRegistry: '0xa78d434b49dd7267c88cbf5ed181598d8290006b',
+            addressRegistry: '0xc8fed8603e0e6bf0d9a6b6c9b89606135f60ed71',
         },
     },
 };
